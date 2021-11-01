3335ce2dc36010893e9bb7f6ae520a1222b56c6a
Arti14MRN <91422101+Arti14MRN@users.noreply.github.com>
2021-11-01 22:43:37 +0900

    add Lesson 2_1
    algorithms 1 lesson

---
+++ Lesson2_1/Lesson2_1_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson2_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Алгоритм вычисления простоты числа
+            int number = 3;
+            int i = 2;
+            if (number % i == 0)
+            {
+                Console.WriteLine("сложное");
+            }
+            else
+            {
+                Console.WriteLine("простое");
+            }
+        }
+    }
+}
+
---
+++ Lesson2_1/Lesson2_1_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lesson2_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.Посчитайте сложность функции
+            //    public static int StrangeSum(int[] inputArray)
+            //    {
+            //        int sum = 0;
+            //        for (int i = 0; i < inputArray.Length; i++) //O(N)
+            //        {
+            //            for (int j = 0; j < inputArray.Length; j++)//O(N)
+            //            {
+            //                for (int k = 0; k < inputArray.Length; k++)//O(N)
+            //                {
+            //                    int y = 0;
+
+            //                    if (j != 0)
+            //                    {
+            //                        y = k / j;
+            //                    }
+
+            //                    sum += inputArray[i] + i + k + j + y;
+            //                }
+            //            }
+            //        }
+
+            //        return sum;
+            //    }
+            //
+            //   
+            Console.WriteLine("сложность функции составляет O(N^3)");
+        }
+    }
+}
